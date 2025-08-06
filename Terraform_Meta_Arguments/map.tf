dc47e70d851ed4c80c9ce287238980abfc1c0506
Anonymous_14 <yogirajgite123@gmail.com>
2025-08-06 23:11:58 +0530

    Create map.tf

---
+++ Terraform_Meta_Arguments/map.tf
@@ -0,0 +1,21 @@
+#Use to provide same tags to multiple resources
+
+resource "aws_instance" "example" {
+  ami  = "ami-0c94855ba95c574c8"
+  instance_type = "t2.micro"
+  tags  = var.tag_values                #It will take tags from below variable values
+
+}
+
+
+
+variable "tag_values" {
+  description = "Map of tags to assign to the resources"
+  type  = map(string)
+  default  = {
+  Environment = "Development"
+  Team  = "DevOps"
+  batch = "26"
+  Class = "Young minds"
+  }
+}
