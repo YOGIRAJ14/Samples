6fae0e9e1591fe7956711e159396dd33309e50ee
Anonymous_14 <yogirajgite123@gmail.com>
2025-08-06 22:58:47 +0530

    Create depends_on.tf

---
+++ Terraform_Meta_Arguments/depends_on.tf
@@ -0,0 +1,16 @@
+
+resource "aws_instance" "example" {
+  ami           = "ami-0123456789abcdef0"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0123456789abcdef0"
+  key_name      = "example-key"
+
+  depends_on = [aws_security_group.example]     # It won't create EC2 w/out sg. Terraform first create security group & then EC2 instance.
+}
+
+
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Example security group"
+  # Security group rules configuration...
+}
