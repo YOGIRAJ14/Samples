9ba8ee4669f2c75698a11c56b7dc925e4563dcfc
Anonymous_14 <yogirajgite123@gmail.com>
2025-08-06 23:19:41 +0530

    Update map.tf

--- Terraform_Meta_Arguments/map.tf
+++ Terraform_Meta_Arguments/map.tf
@@ -13,9 +13,9 @@ variable "tag_values" {
   description = "Map of tags to assign to the resources"
   type  = map(string)
   default  = {
-  Environment = "Development"
-  Team  = "DevOps"
-  batch = "26"
-  Class = "Young minds"
+  Environment = "Dev"
+  Team  = "Cloudengg"
+  Cloudtype = "aws"
+  os = "Ubuntu"
   }
 }
